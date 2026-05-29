d6fa120141d277252dd4490f5fec00f50c8203df
Milan van den broek <D319333@edu.curio.nl>
2026-05-29 14:22:38 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html class="html-home" lang="nl">
+
+<head>
+    <title>Homepage | Pretpark Webapp</title>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<?php require_once 'header.php'; ?>
+<body class="home-body">
+    
+    <div class="wrapper">
+        <div class="h1_homepage">
+            <h1>Welkom bij de Pretpark Webapp</h1>
+        </div>
+        
+    </div>
+
+    <div class="wrapper-main">
+        <div class="container-home">
+            <div class="container">
+                <p>Dit is onze pretpark site waar je je eigen taken kan aanpassen of aanmaken!</p>
+            </div>
+        </div>
+    </div>
+
+    >
+</body>
+<footer>
+        <p>&copy; Hannes | Milan | Ruben | Timothy</p>
+</footer>
+
+</html>
