fc2337318b74eb636a631176cb36af710960c85a
Milan van den broek <D319333@edu.curio.nl>
2026-06-01 11:44:44 +0200

    header

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="header-container">
+        <a href="/" class="logo">Fotogalerij</a>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="fotogalerij.php">Fotogalerij</a>
+            <a href="contact.php">Contact</a>
+            <span class="cart-icon">🛒</span>
+        </nav>
+    </div>
+</header>
