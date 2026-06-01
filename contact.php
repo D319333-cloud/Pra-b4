0fb616590754adc2194872e08aceeb4448701e55
beaulauwerijssen <bjl@ziggo.nl>
2026-06-01 11:09:06 +0200

    contact pagina

---
+++ contact.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fotogalerij - Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <a href="/" class="logo">Fotogalerij</a>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="fotogalerij.php">Fotogalerij</a>
+                <a href="contact.php">Contact</a>
+                <span class="cart-icon">🛒</span>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <section class="">
+            <div class="hero-content">
+            </div>
+        </section>
+
+        
+    </main>
+
+    <footer>
+        <p>&copy; 2026 Fotogalerij. Alle rechten voorbehouden.</p>
+        <p>
+            <a href="contact.php">Contact</a>
+        </p>
+    </footer>
+</body>
+</html>
